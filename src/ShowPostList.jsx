f9a2e62d467ac7bf5c84c69d3f23293c1a656f55
bookhabit <qnrgoqlt@naver.com>
2022-07-30 14:54:34 +0900

    pagination : 페이지 넘겨주는 기능 구현

--- src/ShowPostList.jsx
+++ src/ShowPostList.jsx
@@ -23,41 +23,59 @@ import EachPost from "./EachPost";
 import axios from "axios";
 
 // 서버에서 데이터를 가져와서 포스팅하기 전 미리 구현을 위한 임의 배열
-const initialPostList = [
-  { id: 1, title: "시사 N 대학기자상 취재" },
-  { id: 2, title: "시사 N 대학기자상 취재" },
-  { id: 3, title: "시사 N 대학기자상 취재" },
-];
+// const initialPostList = [
+//   { id: 1, title: "시사 N 대학기자상 취재" },
+//   { id: 2, title: "시사 N 대학기자상 취재" },
+//   { id: 3, title: "시사 N 대학기자상 취재" },
+// ];
 
 const ShowPostList = ({ apiUrl }) => {
   const [loading, setLoading] = useState(true);
-  const [isPost, setIsPost] = useState(false);
+  // const [isPost, setIsPost] = useState(false);
   const [postList, setPostList] = useState([]);
+  const [page, setPage] = useState(1);
+  const [pages, setPages] = useState([]);
+  const [totalLastPage, setTotalLastPage] = useState();
 
-  const addPost = useCallback(() => {
-    setPostList((postList) => [
-      ...postList,
-      { id: 4, title: "시사 N 대학기자상 취재" },
-    ]);
-  }, [postList]);
+  const getPostList = useCallback(() => {
+    setLoading(true);
+    axios.get(`${apiUrl}list/?page=${page}&page_size=10`).then((response) => {
+      console.log(response.data);
+      setTotalLastPage(response.data.count);
+      const lastPage = Math.ceil(response.data.count / 30); // 전체 페이지 개수
+      const tempPages = [];
+      for (let i = 1; i <= lastPage; i++) {
+        tempPages.push(i);
+      }
+      setPages(tempPages);
+
+      setPostList(response.data.results);
+      setLoading(false);
+    });
+  });
+
+  // const addPost = useCallback(() => {
+  //   setPostList((postList) => [
+  //     ...postList,
+  //     { id: 4, title: "시사 N 대학기자상 취재" },
+  //   ]);
+  // }, [postList]);
 
   const navigate = useNavigate();
   const goWirte = () => {
     navigate("/write");
   };
-  useEffect(() => {
-    axios.get(`${apiUrl}list/?page=1&page_size=10`).then((response) => {
-      console.log(response.data);
-      setPostList(response.data.results);
-      setLoading(false);
-    });
-  }, []); // 렌더링되고 처음 한번만 실행되도록 빈배열 넣어줌
+  useEffect(getPostList, [page]);
 
   return (
     <>
       <PostSection>
         <PostTitleDiv>
-          <FontAwesomeIcon onClick={addPost} icon={faArrowsRotate} />
+          <FontAwesomeIcon
+            style={{ cursor: "pointer" }}
+            onClick={getPostList}
+            icon={faArrowsRotate}
+          />
           <PostTitle>익명게시판</PostTitle>
           <CursorDiv>
             <FontAwesomeIcon onClick={goWirte} icon={faPenToSquare} />
@@ -69,7 +87,7 @@ const ShowPostList = ({ apiUrl }) => {
               로딩중
               <LoadingImg src={loadingIcon} />
             </LoadingDiv>
-          ) : isPost ? (
+          ) : postList.length === 0 ? (
             <LoadingDiv>기록된 글이 없습니다</LoadingDiv>
           ) : (
             <ul>
@@ -84,12 +102,35 @@ const ShowPostList = ({ apiUrl }) => {
           )}
         </PostListDiv>
       </PostSection>
-      <PagingSection>
-        <PagenumberDiv>
+      <PagingSection style={{ cursor: "pointer" }}>
+        <PagenumberDiv
+          onClick={() => {
+            if (page > 1) {
+              setPage(page - 1);
+            }
+          }}
+        >
           <FontAwesomeIcon icon={faArrowLeft} />
         </PagenumberDiv>
-        <PagenumberDiv>2</PagenumberDiv>
-        <PagenumberDiv>
+        {pages.map((pageNum) => (
+          <PagenumberDiv
+            key={pageNum}
+            onClick={() => {
+              setPage(pageNum);
+              //console.log(pageNum);
+            }}
+          >
+            {pageNum}
+          </PagenumberDiv>
+        ))}
+        <PagenumberDiv
+          onClick={() => {
+            console.log(totalLastPage);
+            if (totalLastPage > page) {
+              setPage(page + 1);
+            }
+          }}
+        >
           <FontAwesomeIcon icon={faArrowRight} />
         </PagenumberDiv>
       </PagingSection>
